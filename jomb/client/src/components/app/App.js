92f13ab2279119f92f3d3fb06385847e611e66b7
Stacy Harrison <stacy.e.harrison@gmail.com>
2018-09-03 09:50:13 -0400

    Moving shared css to App.css, establishing basic flex layout

--- jomb/client/src/components/app/App.js
+++ jomb/client/src/components/app/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import BookList from "../booklist/BookList";
-import "../../shared/_layout.css";
+import "normalize.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div id="Jomb">
-        <p>Hello world</p>
-        <BookList />
+        <header>
+          <div id="SiteTitle">
+            <h1>just organize my books</h1>
+          </div>
+          <div id="SiteNav">
+            <a href="/add" class="top-nav">
+              add
+            </a>
+          </div>
+          <div id="HeaderRight">
+            <a href="/login" id="LoginLink">
+              login
+            </a>
+          </div>
+        </header>
+        <div id="Main">
+          <div id="SideBar">
+            <BookList />
+          </div>
+          <div id="BookDetail">
+            <p>placeholder</p>
+          </div>
+        </div>
       </div>
     );
   }
