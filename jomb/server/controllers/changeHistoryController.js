76a409722497f3cb3c3bfaf2c6a87d4bacae5f17
Stacy Harrison <stacy.e.harrison@gmail.com>
2018-09-20 19:16:01 -0400

    Refactoring to simplify mongoose calls

--- jomb/server/controllers/changeHistoryController.js
+++ jomb/server/controllers/changeHistoryController.js
@@ -3,7 +3,7 @@ const ChangeHistory = require("../models/changeHistory");
 const Book = require("../models/book");
 
 const getAllChanges = async (req, res) => {
-  const changes = ChangeHistory.find({}, (err, changes) => {
+  await ChangeHistory.find({}, (err, changes) => {
     if (err) {
       return res.json({ message: "Error retrieving all change histories" });
     }
@@ -12,19 +12,16 @@ const getAllChanges = async (req, res) => {
 };
 
 const getChangesForBook = async (req, res) => {
-  const changes = await ChangeHistory.find(
-    { bookId: req.params.bookId },
-    (err, changes) => {
-      if (err) {
-        return res.json({
-          message: `Error retrieving all change histories for ${
-            req.params.bookId
-          }`
-        });
-      }
-      res.json(changes);
+  await ChangeHistory.find({ bookId: req.params.bookId }, (err, changes) => {
+    if (err) {
+      return res.json({
+        message: `Error retrieving all change histories for ${
+          req.params.bookId
+        }`
+      });
     }
-  );
+    res.json(changes);
+  });
 };
 
 module.exports = { addChangeHistoryToBook, getAllChanges, getChangesForBook };
